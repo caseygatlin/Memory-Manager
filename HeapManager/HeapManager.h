4e5a087f503941f8252fa09db5fb2dd45c163c02
Casey Gatlin <u1274838@utah.edu>
2019-10-18 00:43:03 -0600

    Increased readability of HeapManager.h

--- HeapManager/HeapManager.h
+++ HeapManager/HeapManager.h
@@ -13,33 +13,33 @@ public:
 	static HeapManager* create(void* i_pHeapMemory, size_t i_heapMemorySize);
 
 	// allocates memory. named with a underscore to resolve name clash
-	void* _alloc(size_t i_bytes);
+	void*	_alloc(size_t i_bytes);
 	// allocation with alignment. returns a block of memory with a given alignment
-	void* _alloc(size_t i_bytes, unsigned int i_alignment);
+	void*	_alloc(size_t i_bytes, unsigned int i_alignment);
 
 	// frees an allocation
-	bool _free(void* i_ptr);
+	bool	_free(void* i_ptr);
 
 	// attempt to merge abutting blocks.
-	void collect();
+	void	collect();
 
 	// tells us the size of the largest free block
-	size_t getLargestFreeBlock();
+	size_t	getLargestFreeBlock();
 	// tells us how much total memory is available
-	size_t getTotalFreeMemory();
+	size_t	getTotalFreeMemory();
 
 	// tells us if a pointer is within this HeapManagers managed memory
-	bool Contains( void * i_ptr);
+	bool	Contains( void * i_ptr);
 	// tells us if this pointer is an outstanding allocation from this block
-	bool IsAllocated(void* i_ptr);
+	bool	IsAllocated(void* i_ptr);
 
 	// a debugging helper function to show us all the free blocks.
-	void ShowFreeBlocks();
+	void	ShowFreeBlocks();
 	// a debugging helper function to show us all the outstanding blocks.
-	void ShowOutstandingAllocations();
+	void	ShowOutstandingAllocations();
 
 	// Destructs the heap manager
-	void destroy();
+	void	destroy();
 
 private:
 	BlockDesc* m_pFreeMemHead;
