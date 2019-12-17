8ac449c43f19ee9009da0c1ccba0e5e93dd4006d
Casey Gatlin <u1274838@utah.edu>
2019-12-17 05:59:44 -0700

    Created basic constructor for BitArray

---
+++ BitArray.cpp
@@ -0,0 +1,26 @@
+#include "BitArray.h"
+#include <stdint.h>
+
+// Constructor
+BitArray::BitArray(const size_t& i_size, void* i_memLocation)
+{
+	m_size = i_size;
+
+
+#ifdef _WIN64
+	m_pBits = static_cast<uint64_t*>(i_memLocation);
+	for (size_t i = 0; i < i_size; i++)
+	{
+		m_pBits[i] = UINT64_MAX;
+	}
+
+#else
+	m_pBits = static_cast<uint32_t*>(i_memLocation);
+	for (size_t i = 0; i < i_size; i++)
+	{
+		m_pBits[i] = UINT32_MAX;
+	}
+
+#endif
+
+}
