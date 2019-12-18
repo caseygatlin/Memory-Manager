8ba9139742c59d3971fbb104783b4572145fae4c
Casey <u1274838@utah.edu>
2019-12-17 23:13:45 -0700

    Started implementation of FSA alloc

--- FixedSizeAllocator.cpp
+++ FixedSizeAllocator.cpp
@@ -1,5 +1,6 @@
 #include "FixedSizeAllocator.h"
 #include "BitArray.h"
+#include <stdint.h>
 
 FixedSizeAllocator::FixedSizeAllocator(const void* i_pHeapMemory, const size_t& i_heapMemorySize)
 {
@@ -22,6 +23,30 @@ FixedSizeAllocator::~FixedSizeAllocator()
 
 }
 
+void* FixedSizeAllocator::_alloc()
+{
+
+	size_t firstAvailable;
+	if (m_pFreeBits->FindFirstSetBit(firstAvailable))
+	{
+
+		m_pFreeBits->ClearBit(firstAvailable);
+		
+		uintptr_t uipFreeMem = reinterpret_cast<uintptr_t>(m_pFreeMem);
+		uintptr_t uipUserPtr = uipFreeMem + (firstAvailable * m_BlockSize);
+		void* pUserPtr = reinterpret_cast<void*>(uipUserPtr);
+
+		return pUserPtr;
+
+	}
+	else
+	{
+
+		return nullptr;
+	
+	}
+
+}
 
 bool FixedSizeAllocator::_free(void* i_ptr)
 {
