208609eda280d597f1cd42f2ccb919e0c888c37b
Casey <u1274838@utah.edu>
2019-12-17 23:10:49 -0700

    Added declarations for methods in FixedSizeAllocator

--- FixedSizeAllocator.h
+++ FixedSizeAllocator.h
@@ -1,9 +1,21 @@
 #pragma once
 
+class BitArray;
+
 class FixedSizeAllocator
 {
 public:
+	FixedSizeAllocator(const void* i_pHeapMemory, const size_t& i_heapMemorySize);
 
+	static FixedSizeAllocator* Create(const void* i_pHeapMemory, const size_t& i_heapMemorySize);
+	~FixedSizeAllocator();
+
+	void* _alloc();
+	bool _free(void* i_ptr);
+	
 
 private:
+	size_t m_BlockSize;
+	void* m_pFreeMem;
+	BitArray* m_pFreeBits;
 };
