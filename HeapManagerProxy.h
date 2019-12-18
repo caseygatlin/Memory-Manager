7360088bdf55a585139f1b6a3e8c984ab20a6b3e
Casey <u1274838@utah.edu>
2019-12-17 21:45:29 -0700

    Added over HeapManager files

---
+++ HeapManagerProxy.h
@@ -0,0 +1,32 @@
+#ifndef __HEAP_MANAGER_PROXY_H
+#define __HEAP_MANAGER_PROXY_H
+
+#include <stdint.h>
+
+class HeapManager;
+
+namespace HeapManagerProxy
+{
+	inline HeapManager *	CreateHeapManager( const void * i_pMemory, const size_t& i_sizeMemory);
+	inline void				Destroy( HeapManager * i_pManager );
+
+	inline void *			alloc( HeapManager * i_pManager, const size_t& i_size );
+	inline void *			alloc( HeapManager * i_pManager, const size_t& i_size, const unsigned int& i_alignment );
+	inline bool				free( HeapManager * i_pManager, void * i_ptr );
+
+	inline void				Collect( HeapManager * i_pManager );
+
+	inline bool				Contains( const HeapManager * i_pManager, const void * i_ptr );
+	inline bool				IsAllocated( const HeapManager * i_pManager, const void * i_ptr );
+
+	inline size_t			GetLargestFreeBlock( const HeapManager * i_pManager );
+	inline size_t			GetTotalFreeMemory( const HeapManager * i_pManager );
+
+	inline void				ShowFreeBlocks( const HeapManager * i_pManager );
+	inline void				ShowOutstandingAllocations( const HeapManager * i_pManager );
+
+} // namespace HeapManagerProxy
+
+#include "HeapManagerProxy-inl.h"
+
+#endif // __HEAP_MANAGER_PROXY_H
---
+++ HeapManager_UnitTest.h
@@ -0,0 +1,4 @@
+#pragma once
+
+bool HeapManager_UnitTest();
+bool HeapManager_UnitTest_Small();
