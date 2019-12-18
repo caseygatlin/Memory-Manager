aba9ad9382d45d237ce7bca456a92b44612d6c56
Casey <u1274838@utah.edu>
2019-12-17 23:12:26 -0700

    Added empty FSA function definitions

--- FixedSizeAllocator.cpp
+++ FixedSizeAllocator.cpp
@@ -1,1 +1,22 @@
 #include "FixedSizeAllocator.h"
+
+FixedSizeAllocator::FixedSizeAllocator(const void* i_pHeapMemory, const size_t& i_heapMemorySize)
+{
+
+}
+
+FixedSizeAllocator* FixedSizeAllocator::Create(const void* i_pHeapMemory, const size_t& i_heapMemorySize)
+{
+	return nullptr;
+}
+
+
+bool FixedSizeAllocator::_free(void* i_ptr)
+{
+	// Check that pointer is a valid pointer within this allocator
+	// And that it's an outstanding allocation
+
+	// Set bit for that block
+
+	return true;
+}
