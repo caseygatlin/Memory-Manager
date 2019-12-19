e28d1b9a667d42ac9a6f88669444bb72374fd4a1
Casey Gatlin <u1274838@utah.edu>
2019-12-19 05:28:24 -0700

    Adjusted FixedSizeAllocatorInitData to new namespaces

---
+++ FinalExam/MemorySystem/DynamicMemory/Public/FixedSizeAllocatorInitData.h
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace memory_system
+{
+	namespace dynamic_memory
+	{
+		struct FixedSizeAllocatorInitData
+		{
+			FixedSizeAllocatorInitData(size_t i_sizeBlock, size_t i_numBlocks) :
+				sizeBlock(i_sizeBlock),
+				numBlocks(i_numBlocks)
+			{}
+
+			size_t sizeBlock;
+			size_t numBlocks;
+		};
+
+		const static unsigned int S_NUM_FSA_INIT = 5;
+		static FixedSizeAllocatorInitData S_FSA_INITS[5] =
+		{
+			FixedSizeAllocatorInitData(8, 20),
+			FixedSizeAllocatorInitData(16, 20),
+			FixedSizeAllocatorInitData(32, 20),
+			FixedSizeAllocatorInitData(64, 20),
+			FixedSizeAllocatorInitData(128, 5)
+		};
+	}
+}
