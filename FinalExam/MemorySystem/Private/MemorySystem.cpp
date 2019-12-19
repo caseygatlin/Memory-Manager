360870c53d08a6b39a2b251da5a3f37633deeffa
Casey Gatlin <u1274838@utah.edu>
2019-12-19 05:28:04 -0700

    Adjusted MemorySystem to new namespaces

---
+++ FinalExam/MemorySystem/Private/MemorySystem.cpp
@@ -0,0 +1,37 @@
+#include "MemorySystem.h"
+#include "FixedSizeAllocatorInitData.h"
+#include "DynamicMemory.h"
+#include "HeapManager.h"
+#include <assert.h>
+
+
+namespace memory_system
+{
+	using namespace dynamic_memory;
+
+	bool InitializeMemorySystem(void* i_pHeapMemory, size_t i_sizeHeapMemory)
+	{
+		S_DEFAULT_HEAP_MANAGER = CreateDefaultHeap(i_pHeapMemory, i_sizeHeapMemory);
+		assert(S_DEFAULT_HEAP_MANAGER);
+
+		for (unsigned int i = 0; i < S_NUM_FSA_INIT; i++)
+		{ 
+			CreateFixedSizeAllocator(S_FSA_INITS[i].sizeBlock, S_FSA_INITS[i].numBlocks, S_DEFAULT_HEAP_MANAGER);
+		}
+
+		return true;
+	}
+
+	void Collect()
+	{
+		// coalesce free blocks
+		// you may or may not need to do this depending on how you've implemented your HeapManager
+	}
+
+	void DestroyMemorySystem()
+	{
+		S_DEFAULT_HEAP_MANAGER->Destroy();
+	}
+
+
+}
--- FinalExam/MemorySystem/Public/MemorySystem.h
+++ FinalExam/MemorySystem/Public/MemorySystem.h
@@ -1,20 +1,16 @@
 #pragma once
 
-class FixedSizeAllocator;
-class HeapManager;
 
-namespace MemorySystemProxy
+namespace memory_system
 {
-	static HeapManager* S_DEFAULT_HEAP_MANAGER;
 
 	// InitializeMemorySystem - initialize your memory system including your HeapManager and some FixedSizeAllocators
 	bool InitializeMemorySystem(void* i_pHeapMemory, size_t i_sizeHeapMemory);
-
-	FixedSizeAllocator* FindFixedSizeAllocator(const size_t& i_sizeBlock);
 
 	// Collect - coalesce free blocks in attempt to create larger blocks
 	void Collect();
 
 	// DestroyMemorySystem - destroy your memory systems
 	void DestroyMemorySystem();
+
 }
