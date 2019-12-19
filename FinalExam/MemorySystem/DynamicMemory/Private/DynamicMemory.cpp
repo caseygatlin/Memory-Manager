c5b6437916d239133ed6cea109a31571beb339f3
Casey Gatlin <u1274838@utah.edu>
2019-12-19 07:18:51 -0700

    Improved readability of DynamicMemory.cpp

--- FinalExam/MemorySystem/DynamicMemory/Private/DynamicMemory.cpp
+++ FinalExam/MemorySystem/DynamicMemory/Private/DynamicMemory.cpp
@@ -19,51 +19,71 @@ namespace memory_system
 {
 	namespace dynamic_memory
 	{
+
 		class HeapManager;
 
 		FixedSizeAllocator* FindFixedSizeAllocator(const size_t& i_sizeBlock)
 		{
+
 			for (size_t i = 0; i < S_NUM_FSA_INIT; i++)
 			{
+
 				if (S_FIXED_SIZE_ALLOCATORS[i]->GetBlockSize() == i_sizeBlock)
 				{
+
 					return S_FIXED_SIZE_ALLOCATORS[i];
+
 				}
+
 			}
 
 			return nullptr;
+
 		}
+
+
 
 		void CreateFixedSizeAllocator(const size_t& i_sizeBlock, const size_t& i_numBlocks, HeapManager* i_pHeapManager)
 		{
+
 			size_t bitArraySize = i_numBlocks / BIT_DIVISOR;
+
 			if (i_numBlocks % BIT_DIVISOR > 0)
 			{
+
 				bitArraySize++;
+
 			}
 
-			size_t bitArrayMemorySize = sizeof(BitArray) + bitArraySize * BIT_DIVISOR;
-			size_t FSAMemorySize = sizeof(FixedSizeAllocator) + bitArrayMemorySize + i_sizeBlock * i_numBlocks;
-			void* pFSAMemoryLocation = i_pHeapManager->_alloc(FSAMemorySize);
+
+			size_t   bitArrayMemorySize   = sizeof(BitArray)           + bitArraySize       * BIT_DIVISOR;
+			size_t   FSAMemorySize        = sizeof(FixedSizeAllocator) + bitArrayMemorySize + i_sizeBlock * i_numBlocks;
+			void*    pFSAMemoryLocation   = i_pHeapManager->_alloc(FSAMemorySize);
+
 
 			S_FIXED_SIZE_ALLOCATORS[S_NUM_FIXED_SIZE_ALLOCATORS] = FixedSizeAllocator::Create(pFSAMemoryLocation, FSAMemorySize, i_numBlocks, i_sizeBlock);
 
 			S_NUM_FIXED_SIZE_ALLOCATORS++;
+
 		}
+
 
 
 		HeapManager* CreateDefaultHeap(void* i_pHeapMemory, size_t i_sizeHeapMemory)
 		{
+
 			if (S_DEFAULT_HEAP_MANAGER == nullptr)
 			{
+
 				assert(i_pHeapMemory);
 
-				HeapManager* pReturnHeap = HeapManager::Create(i_pHeapMemory, i_sizeHeapMemory);
+				HeapManager*   pReturnHeap = HeapManager::Create(i_pHeapMemory, i_sizeHeapMemory);
+				return         pReturnHeap;
 
-				return pReturnHeap;
 			}
 
 			return nullptr;
+
 		}
 	}
 }
