c9612766b152fb132ec0c9905e6ddc2baa1c0fb0
Casey Gatlin <u1274838@utah.edu>
2019-12-20 03:31:23 -0700

    Increased readability of BitArray.h

--- FinalExam/MemorySystem/DynamicMemory/Public/BitArray.h
+++ FinalExam/MemorySystem/DynamicMemory/Public/BitArray.h
@@ -3,12 +3,15 @@
 #include <stdint.h>
 
 
+
 class FixedSizeAllocator;
+
 
 class BitArray
 {
 public:
 
+    // Creator
 	static BitArray* Create(const size_t& i_numBits, const void* i_memLocation);
 
 	// Constructor
@@ -17,35 +20,38 @@ public:
 	// Destructor
 	~BitArray();
 
+
+    // Clear / Set all bits
 	void ClearAll();
 	void SetAll();
 
+
+    // Check a bit
 	bool IsBitSet(size_t i_bitIndex) const;
 	bool IsBitClear(size_t i_bitIndex) const;
 
+
+    // Change a bit
 	void SetBit(size_t i_bitIndex);
 	void ClearBit(size_t i_bitIndex);
 
 
-	// Finds and outputs index of first set bit
+	// Finds and outputs index of first set / clear bit
 	bool FindFirstSetBit(size_t& o_bitIndex) const;
 	bool FindFirstClearBit(size_t& o_bitIndex) const;
 
-	bool GetNextSetBit(size_t& o_bitIndex) const;
-	bool GetNextClearBit(size_t& o_bitIndex) const;
-
-	// Operator[]
 
 private:
 
 	// Array of bits depending on architecture
 #ifdef _WIN64
-	uint64_t* m_pBits;
+	uint64_t*   m_pBits;
 #else
-	uint32_t* m_pBits;
+	uint32_t*   m_pBits;
 #endif
 
+
 	// Size of the array
-	size_t m_size;
+	size_t      m_size;
 
 };
