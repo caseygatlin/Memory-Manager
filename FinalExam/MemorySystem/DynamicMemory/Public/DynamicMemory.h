5c0f7a0ec63a9bd6e54e58a1d746822aea6859b1
Casey Gatlin <u1274838@utah.edu>
2019-12-20 03:31:42 -0700

    Increased readability of DynamicMemory.h

--- FinalExam/MemorySystem/DynamicMemory/Public/DynamicMemory.h
+++ FinalExam/MemorySystem/DynamicMemory/Public/DynamicMemory.h
@@ -4,11 +4,22 @@
 class FixedSizeAllocator;
 class HeapManager;
 
-HeapManager*	GetDefaultHeapManager();
+
+// --- Methods for accessing / creating memory managers --- //
 
 
-
+// Create an FSA
 void					CreateFixedSizeAllocator(const size_t& i_sizeBlock, const size_t& i_numBlocks, HeapManager* i_pHeapManager);
+
+
+// Create Default Heap Manager
 HeapManager*			CreateDefaultHeap(void* i_pHeapMemory, size_t i_sizeHeapMemory);
+
+
+// Get Default Heap Manager
+HeapManager*            GetDefaultHeapManager();
+
+
+// Get an FSA that can allocate blocks of a given size
 FixedSizeAllocator*		FindFixedSizeAllocator(const size_t& i_sizeBlock);
 
