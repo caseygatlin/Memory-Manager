3b888e7a5d377dbdce263f38768ed8d469cbb8a2
Casey Gatlin <u1274838@utah.edu>
2019-12-19 21:48:18 -0700

    Indentation changes in BitArray.h

--- FinalExam/MemorySystem/DynamicMemory/Public/BitArray.h
+++ FinalExam/MemorySystem/DynamicMemory/Public/BitArray.h
@@ -3,49 +3,49 @@
 #include <stdint.h>
 
 
-		class FixedSizeAllocator;
+class FixedSizeAllocator;
 
-		class BitArray
-		{
-		public:
+class BitArray
+{
+public:
 
-			static BitArray* Create(const size_t& i_numBits, const void* i_memLocation);
+	static BitArray* Create(const size_t& i_numBits, const void* i_memLocation);
 
-			// Constructor
-			BitArray(const size_t& i_size, void* i_memLocation);
+	// Constructor
+	BitArray(const size_t& i_size, void* i_memLocation);
 
-			// Destructor
-			~BitArray();
+	// Destructor
+	~BitArray();
 
-			void ClearAll();
-			void SetAll();
+	void ClearAll();
+	void SetAll();
 
-			bool IsBitSet(size_t i_bitIndex) const;
-			bool IsBitClear(size_t i_bitIndex) const;
+	bool IsBitSet(size_t i_bitIndex) const;
+	bool IsBitClear(size_t i_bitIndex) const;
 
-			void SetBit(size_t i_bitIndex);
-			void ClearBit(size_t i_bitIndex);
+	void SetBit(size_t i_bitIndex);
+	void ClearBit(size_t i_bitIndex);
 
 
-			// Finds and outputs index of first set bit
-			bool FindFirstSetBit(size_t& o_bitIndex) const;
-			bool FindFirstClearBit(size_t& o_bitIndex) const;
+	// Finds and outputs index of first set bit
+	bool FindFirstSetBit(size_t& o_bitIndex) const;
+	bool FindFirstClearBit(size_t& o_bitIndex) const;
 
-			bool GetNextSetBit(size_t& o_bitIndex) const;
-			bool GetNextClearBit(size_t& o_bitIndex) const;
+	bool GetNextSetBit(size_t& o_bitIndex) const;
+	bool GetNextClearBit(size_t& o_bitIndex) const;
 
-			// Operator[]
+	// Operator[]
 
-		private:
+private:
 
-			// Array of bits depending on architecture
+	// Array of bits depending on architecture
 #ifdef _WIN64
-			uint64_t* m_pBits;
+	uint64_t* m_pBits;
 #else
-			uint32_t* m_pBits;
+	uint32_t* m_pBits;
 #endif
 
-			// Size of the array
-			size_t m_size;
+	// Size of the array
+	size_t m_size;
 
-		};
+};
