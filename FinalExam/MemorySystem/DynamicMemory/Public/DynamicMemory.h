b45ef69de02ae7e944de2e4fd704094d01efb9fa
Casey Gatlin <u1274838@utah.edu>
2019-12-19 21:44:38 -0700

    DynamicMemory.h uses HeapManagerProxy

--- FinalExam/MemorySystem/DynamicMemory/Public/DynamicMemory.h
+++ FinalExam/MemorySystem/DynamicMemory/Public/DynamicMemory.h
@@ -1,14 +1,16 @@
 #pragma once
 
 
-		class FixedSizeAllocator;
-		class HeapManager;
+class FixedSizeAllocator;
+class HeapManager;
 
-		static HeapManager*				S_DEFAULT_HEAP_MANAGER = nullptr;
-		static FixedSizeAllocator*		S_FIXED_SIZE_ALLOCATORS[5] = { nullptr, nullptr, nullptr, nullptr, nullptr };
-		static size_t					S_NUM_FIXED_SIZE_ALLOCATORS = 0;
+static			HeapManager* S_DEFAULT_HEAP_MANAGER = nullptr;
+HeapManager*	GetDefaultHeapManager();
 
-		void					CreateFixedSizeAllocator(const size_t& i_sizeBlock, const size_t& i_numBlocks, HeapManager* i_pHeapManager);
-		HeapManager*			CreateDefaultHeap(void* i_pHeapMemory, size_t i_sizeHeapMemory);
-		FixedSizeAllocator*		FindFixedSizeAllocator(const size_t& i_sizeBlock);
+static FixedSizeAllocator*		S_FIXED_SIZE_ALLOCATORS[5] = { nullptr, nullptr, nullptr, nullptr, nullptr };
+static size_t					S_NUM_FIXED_SIZE_ALLOCATORS = 0;
+
+void					CreateFixedSizeAllocator(const size_t& i_sizeBlock, const size_t& i_numBlocks, HeapManager* i_pHeapManager);
+HeapManager*			CreateDefaultHeap(void* i_pHeapMemory, size_t i_sizeHeapMemory);
+FixedSizeAllocator*		FindFixedSizeAllocator(const size_t& i_sizeBlock);
 
