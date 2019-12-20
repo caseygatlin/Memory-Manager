073c2bb11ea7c06f251714480a46aaff397c55d0
Casey Gatlin <u1274838@utah.edu>
2019-12-19 21:48:33 -0700

    Took BlockDesc out of namespaces

--- FinalExam/MemorySystem/DynamicMemory/Public/BlockDesc.h
+++ FinalExam/MemorySystem/DynamicMemory/Public/BlockDesc.h
@@ -2,18 +2,12 @@
 
 #include <stdint.h>
 
-namespace memory_system
-{
-	namespace dynamic_memory
-	{
 
-		// Block descriptor used for keeping track of allocated blocks of memory
-		struct BlockDesc
-		{
-			void*		m_pBlockBase;	// The allocated memory address in the heap manager
-			BlockDesc*	m_pNext;		// Pointer to the next block
-			BlockDesc*	m_pPrev;		// Pointer to the previous block
-			size_t		m_sizeBlock;	// The size of this block
-		};
-	}
-}
+// Block descriptor used for keeping track of allocated blocks of memory
+struct BlockDesc
+{
+	void*		m_pBlockBase;	// The allocated memory address in the heap manager
+	BlockDesc*	m_pNext;		// Pointer to the next block
+	BlockDesc*	m_pPrev;		// Pointer to the previous block
+	size_t		m_sizeBlock;	// The size of this block
+};
