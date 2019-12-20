ec3bfbd1dcb2dad9a0717d1f41f0264bd64a73fd
Casey Gatlin <u1274838@utah.edu>
2019-12-19 21:36:25 -0700

    Memory system uses HeapManagerProxy

--- FinalExam/MemorySystem/Private/MemorySystem.cpp
+++ FinalExam/MemorySystem/Private/MemorySystem.cpp
@@ -1,33 +1,33 @@
 #include "MemorySystem.h"
 #include "FixedSizeAllocatorInitData.h"
 #include "DynamicMemory.h"
-#include "HeapManager.h"
+#include "HeapManagerProxy.h"
 #include <assert.h>
 
 
 
-	bool InitializeMemorySystem(void* i_pHeapMemory, size_t i_sizeHeapMemory)
-	{
-		S_DEFAULT_HEAP_MANAGER = CreateDefaultHeap(i_pHeapMemory, i_sizeHeapMemory);
-		assert(S_DEFAULT_HEAP_MANAGER);
+bool InitializeMemorySystem(void* i_pHeapMemory, size_t i_sizeHeapMemory)
+{
+	S_DEFAULT_HEAP_MANAGER = CreateDefaultHeap(i_pHeapMemory, i_sizeHeapMemory);
+	assert(S_DEFAULT_HEAP_MANAGER);
 
-		for (unsigned int i = 0; i < S_NUM_FSA_INIT; i++)
-		{ 
-			CreateFixedSizeAllocator(S_FSA_INITS[i].sizeBlock, S_FSA_INITS[i].numBlocks, S_DEFAULT_HEAP_MANAGER);
-		}
-
-		return true;
+	for (unsigned int i = 0; i < S_NUM_FSA_INIT; i++)
+	{ 
+		CreateFixedSizeAllocator(S_FSA_INITS[i].sizeBlock, S_FSA_INITS[i].numBlocks, S_DEFAULT_HEAP_MANAGER);
 	}
 
-	void Collect()
-	{
-		// coalesce free blocks
-		// you may or may not need to do this depending on how you've implemented your HeapManager
-	}
+	return true;
+}
 
-	void DestroyMemorySystem()
-	{
-		S_DEFAULT_HEAP_MANAGER->Destroy();
-	}
+void Collect()
+{
+// coalesce free blocks
+// you may or may not need to do this depending on how you've implemented your HeapManager
+}
+
+void DestroyMemorySystem()
+{
+	HeapManagerProxy::Destroy(GetDefaultHeapManager());
+}
 
 
