3dd1b7b7bdb7a23ecd4324b9f9349ddd60719b4e
Casey Gatlin <u1274838@utah.edu>
2019-12-20 03:32:03 -0700

    Increased readability of FSA Init Data file

--- FinalExam/MemorySystem/DynamicMemory/Public/FixedSizeAllocatorInitData.h
+++ FinalExam/MemorySystem/DynamicMemory/Public/FixedSizeAllocatorInitData.h
@@ -2,23 +2,35 @@
 
 #include <stdint.h>
 
+
+// Stores data for initializing FSA's
 struct FixedSizeAllocatorInitData
 {
+
+    // Constructor
 	FixedSizeAllocatorInitData(size_t i_sizeBlock, size_t i_numBlocks) :
 		sizeBlock(i_sizeBlock),
 		numBlocks(i_numBlocks)
 	{}
 
+
+    // Size of block and how many
 	size_t sizeBlock;
 	size_t numBlocks;
 };
 
-const static unsigned int S_NUM_FSA_INIT = 5;
-static FixedSizeAllocatorInitData S_FSA_INITS[5] =
+
+
+// Total number of FSA's to create
+const static unsigned int S_NUM_FSA_INIT = 3;
+
+
+// Data to initialize FSA's
+static FixedSizeAllocatorInitData S_FSA_INITS[3] =
 {
-	FixedSizeAllocatorInitData(8, 20),
-	FixedSizeAllocatorInitData(16, 20),
-	FixedSizeAllocatorInitData(32, 20),
-	FixedSizeAllocatorInitData(64, 20),
-	FixedSizeAllocatorInitData(128, 5)
+
+	FixedSizeAllocatorInitData(16, 100),
+	FixedSizeAllocatorInitData(32, 200),
+	FixedSizeAllocatorInitData(96, 400)
+
 };
