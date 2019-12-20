288069291a7c96fd4268a30c4328ee5a915439fb
Casey Gatlin <u1274838@utah.edu>
2019-12-20 03:30:45 -0700

    Increased readability of DynamicMemory.cpp

--- FinalExam/MemorySystem/DynamicMemory/Private/DynamicMemory.cpp
+++ FinalExam/MemorySystem/DynamicMemory/Private/DynamicMemory.cpp
@@ -5,48 +5,40 @@
 #include "HeapManagerProxy.h"
 #include <assert.h>
 
+
+// Defines preprocessor constants depending on 64 or 32 bit architecture
 #ifdef _WIN64
+
 #ifndef BIT_DIVISOR
 #define BIT_DIVISOR 64
 #endif // !BIT_DIVISOR
+
 #else
+
 #ifndef BIT_DIVISOR
 #define BIT_DIVISOR 32
 #endif // !BIT_DIVISOR
+
 #endif // _WIN64
+
 
 
 class HeapManager;
 class FixedSizeAllocator;
 
-extern HeapManager* S_DEFAULT_HEAP_MANAGER = nullptr;
-extern FixedSizeAllocator* S_FIXED_SIZE_ALLOCATORS[5] = { nullptr, nullptr, nullptr, nullptr, nullptr };
-extern size_t					S_NUM_FIXED_SIZE_ALLOCATORS = 0;
 
-FixedSizeAllocator* FindFixedSizeAllocator(const size_t& i_sizeBlock)
-{
-
-	for (size_t i = 0; i < S_NUM_FSA_INIT; i++)
-	{
-
-		if (S_FIXED_SIZE_ALLOCATORS[i]->GetBlockSize() == i_sizeBlock)
-		{
-
-			return S_FIXED_SIZE_ALLOCATORS[i];
-
-		}
-
-	}
-
-	return nullptr;
-
-}
+// Define external variables that hold the memory managers
+extern HeapManager* S_DEFAULT_HEAP_MANAGER              = nullptr;
+extern FixedSizeAllocator* S_FIXED_SIZE_ALLOCATORS[3]   = { nullptr, nullptr, nullptr };
+extern size_t S_NUM_FIXED_SIZE_ALLOCATORS               = 0;
 
 
 
+// Creates and constructs an FSA for a certain size block
 void CreateFixedSizeAllocator(const size_t& i_sizeBlock, const size_t& i_numBlocks, HeapManager* i_pHeapManager)
 {
 
+    // Correctly size the bitArray
 	size_t bitArraySize = i_numBlocks / BIT_DIVISOR;
 
 	if (i_numBlocks % BIT_DIVISOR > 0)
@@ -57,19 +49,24 @@ void CreateFixedSizeAllocator(const size_t& i_sizeBlock, const size_t& i_numBloc
 	}
 
 
+    // Calculate memory needed
 	size_t   bitArrayMemorySize   = sizeof(BitArray)           + bitArraySize       * BIT_DIVISOR;
 	size_t   FSAMemorySize        = sizeof(FixedSizeAllocator) + bitArrayMemorySize + i_sizeBlock * i_numBlocks;
+
+
+    // Allocate memory
 	void* pFSAMemoryLocation = HeapManagerProxy::alloc(i_pHeapManager, FSAMemorySize);
 
 
+    // Construct an FSA at that memory location
 	S_FIXED_SIZE_ALLOCATORS[S_NUM_FIXED_SIZE_ALLOCATORS] = FixedSizeAllocator::Create(pFSAMemoryLocation, FSAMemorySize, i_numBlocks, i_sizeBlock);
-
 	S_NUM_FIXED_SIZE_ALLOCATORS++;
 
 }
 
 
 
+// Creates, constructs, and returns the Default Heap Manager
 HeapManager* CreateDefaultHeap(void* i_pHeapMemory, size_t i_sizeHeapMemory)
 {
 
@@ -78,6 +75,7 @@ HeapManager* CreateDefaultHeap(void* i_pHeapMemory, size_t i_sizeHeapMemory)
 
 		assert(i_pHeapMemory);
 
+        // Create and return Heap Manager
 		HeapManager*   pReturnHeap = HeapManagerProxy::CreateHeapManager(i_pHeapMemory, i_sizeHeapMemory);
 		return         pReturnHeap;
 
@@ -87,8 +85,32 @@ HeapManager* CreateDefaultHeap(void* i_pHeapMemory, size_t i_sizeHeapMemory)
 
 }
 
+
+
+// Returns Default Heap Manager
 HeapManager* GetDefaultHeapManager()
 {
 	return S_DEFAULT_HEAP_MANAGER;
 }
 
+
+
+// Find and return a fixed size allocator that should allocate a certain size block
+FixedSizeAllocator* FindFixedSizeAllocator(const size_t& i_sizeBlock)
+{
+    // Cycle through the FSA's and return one if possible
+    for (size_t i = 0; i < S_NUM_FSA_INIT; i++)
+    {
+
+        if (S_FIXED_SIZE_ALLOCATORS[i]->GetBlockSize() == i_sizeBlock)
+        {
+
+            return S_FIXED_SIZE_ALLOCATORS[i];
+
+        }
+
+    }
+
+    return nullptr;
+
+}
